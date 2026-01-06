6a76e732f9452d7f0305753199a4f126b3461087
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-06 09:11:53 -0800

    Add the policy to the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Note;
+use App\Policies\NotePolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    protected $policies = [
+        Note::class => NotePolicy::class,
+    ];
 
     /**
      * Bootstrap any application services.
